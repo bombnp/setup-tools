a021352cda12782fa24f9c73f5dc1c5d3077bb79
Nut Pinyo <bomb.NP@gmail.com>
2024-03-14 16:55:02 +0700

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -10,21 +10,21 @@ then
 fi
 
 echo "$0: Installing required packages..."
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
+sudo apt-get install ca-certificates curl
 
 echo "$0: Adding Docker's official GPG key..."
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources:
 echo "$0: Setting up stable repository..."
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "$0: Installing Docker Engine..."
 sudo apt-get update
